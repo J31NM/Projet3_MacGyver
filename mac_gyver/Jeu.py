8362643d6658c03466d99622504d48ac840b3652
Jean Maffert <jeanmaffert@yahoo.fr>
2019-04-10 14:13:08 +0200

    Ajout du dossier mac_gyver

---
+++ mac_gyver/Jeu.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# -*- coding: Utf-8 -*
+
+"""
+Jeu  Mcgyver doit s'échapper du labyrinthe.
+McGyver se trouve dans un labyrinthe dont la sortie est surveillée par un gardien.
+La seule solution pour lui de s'en sortir est de trouver et ramasser une aiguille, un tube et de l'ether,
+dispersés dans le labyrinthe afin de confectionner une seringue qui paralisera le gardien.
+
+Script Python
+Fichiers : jeu.py, requirements.py, structure.py, + images
+"""
+
+import pygame
+from pygame.locals import *
+
+from requirements import *
+
+
+pygame.init()
+
+
+
+
---
+++ mac_gyver/structure.py
@@ -0,0 +1,17 @@
+# Structure du Labyrinthe
+
+Dm0m0m0m0m0m00mn/
+00000000000mm0mn/
+0mmmmmm0m000000n/
+00m00000mm0m0m0n/
+m00mmmmmm0m00m0n/
+0m00m000000m000n/
+m0m0mmmmm0m0mm0n/
+0000m000m0m0m00n/
+0mmmm0m0m0mmmm0n/
+0000m0m000m000mn/
+0mm0m0m00000mmmn/
+0m00m0mmmm000mmn/
+0m0mm0000mmm0mmn/
+0m00mmmm0m0m000n/
+m0000000000mmmAn/
