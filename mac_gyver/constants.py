b2076fc247533307d13777e4b588c832ff519b3b
Jean Maffert <jeanmaffert@yahoo.fr>
2019-05-10 12:22:47 +0200

    New file containing the game constants

---
+++ mac_gyver/constants.py
@@ -0,0 +1,19 @@
+"""
+Global constants
+
+"""
+
+# Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+
+# Screen dimensions
+SCREEN_WIDTH = 750
+SCREEN_HEIGHT = 750
+
+sprites_side = 15
+sprite_size = 50
+window_side = sprites_side * sprite_size
+
+
