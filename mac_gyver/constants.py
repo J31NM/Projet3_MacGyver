f17e2705219ce0542ac73a138de8ad54edd09c68
Jean Maffert <jeanmaffert@yahoo.fr>
2019-06-08 14:38:11 +0200

    Add updated constants.py doc with the messages for the player

--- mac_gyver/constants.py
+++ mac_gyver/constants.py
@@ -10,10 +10,45 @@ BLUE = (0, 0, 255)
 
 # Screen dimensions
 SCREEN_WIDTH = 750
-SCREEN_HEIGHT = 750
+SCREEN_HEIGHT = 800
 
-sprites_side = 15
+# Sprites attributes
+sprites_height = 16
+sprites_width = 15
 sprite_size = 50
-window_side = sprites_side * sprite_size
+window_height = sprites_height * sprite_size
+window_width = sprites_width * sprite_size
 
-
+# Texts for the player
+MESSAGES = {
+    'Intro': [
+        "Try to find the exit, but be careful, the keeper ",
+        "won't let you pass."
+    ],
+    'first_item': [
+        "You found something but that seems useless."
+    ],
+    'second_item': [
+        "Something else ! But still nothing interesting...",
+        "keep searching."
+    ],
+    'syringe': [
+        "Well done !! you build a drowsy syringe.",
+        "You may have a chance against the keeper !"
+    ],
+    'victory1': [
+        "YOU MADE IT ! ",
+        "Come find me at the Phoenix foundation handsome ;)"
+    ],
+    'victory2' : [
+        "Looking for Penny, Macgyver lost himself in the building",
+        "Let's free him in FREE MACGYVER II ! Coming soon !"
+    ],
+    'fail1': [
+        "Oh noo... Too bad",
+        "Your fists were not enough to beat the keeper."
+    ],
+    'fail2': [
+        "MacGyver was brought in jail",
+        "Let's free him in FREE MACGYVER II ! Coming soon !"
+    ]}
